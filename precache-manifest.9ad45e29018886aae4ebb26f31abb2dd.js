99d5ccd0bddfc7bbef8a2c019444a4de6327985e
masesm5@yahoo.com <maseskrikorian@gmail.com>
2021-03-30 21:54:28 -0700

    add missing file

---
+++ precache-manifest.9ad45e29018886aae4ebb26f31abb2dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6bdaf406eb0b4a37405bd981e43a0e",
+    "url": "/masesk-com/index.html"
+  },
+  {
+    "revision": "0f90ea6edc6a97e99995",
+    "url": "/masesk-com/static/css/2.9f59e212.chunk.css"
+  },
+  {
+    "revision": "fb35d95cb6c0dc189fe5",
+    "url": "/masesk-com/static/css/main.c6d714eb.chunk.css"
+  },
+  {
+    "revision": "0f90ea6edc6a97e99995",
+    "url": "/masesk-com/static/js/2.4e3347b9.chunk.js"
+  },
+  {
+    "revision": "fb8f8e6c03826abff168cd1ae48bd2d5",
+    "url": "/masesk-com/static/js/2.4e3347b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb35d95cb6c0dc189fe5",
+    "url": "/masesk-com/static/js/main.5d85c881.chunk.js"
+  },
+  {
+    "revision": "0f4ccabda644da9668f9",
+    "url": "/masesk-com/static/js/runtime-main.bb9aee5e.js"
+  }
+]);
